70c48d3bd1ef282fb064e6350dbf550d96406152
25HP3408 25HP3408 <25HP3408@G301-CC0192.local>
2025-09-26 18:36:06 +0800

    comment

--- app.mjs
+++ app.mjs
@@ -1,6 +1,10 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const UserModel = mongoose.model("User", {
   username: String,
@@ -12,11 +16,14 @@ const UserModel = mongoose.model("User", {
 
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$/;
-const PORT = 4000;
+
+const PORT = 5500;
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hi mom");
@@ -51,30 +58,35 @@ app.post("/signup", async (req, res) => {
   }
 
   if (!email && !phone) {
-    return res
-      .status(400)
-      .send({ message: "Credential must be Email or Phone number!" });
+    return res.status(400).send({ message: "Credential must be Email or Phone number!" });
+  }
+
+  if (email) {
+    const existingUserByEmail = await UserModel.findOne({ email: email });
+    if (existingUserByEmail) {
+      return res.status(400).send({ message: "User with this email already exists" });
+    }
+  }
+
+  if (phone) {
+    const existingUserByPhone = await UserModel.findOne({ phone: phone });
+    if (existingUserByPhone) {
+      return res.status(400).send({ message: "User with this phone already exists" });
+    }
   }
 
   if (body.password.length < 8) {
-    return res
-      .status(400)
-      .send({ message: "Password must be greater than 8 characters" });
+    return res.status(400).send({ message: "Password must be greater than 8 characters" });
   }
 
   if (passwordRegex.test(body.password)) {
-    return res.status(400).send({
-      message:
-        "Password must include Upper and Lowercase letters and digit with special characters",
-    });
+    return res.status(400).send({ message: "Password must include Upper and Lowercase letters and digit with special characters" });
   }
 
   const existingUser = await UserModel.findOne({ username: body.username });
 
   if (existingUser) {
-    return res.status(400).send({
-      message: `User with username "${body.username}" already exists`,
-    });
+    return res.status(400).send({ message: `User with username "${body.username}" already exists` });
   }
 
   const hashedPassword = bcrypt.hashSync(body.password, 10);
@@ -109,17 +121,15 @@ app.post("/signin", async (req, res) => {
   //   return item.email === body.credential || item.phone === body.credential || item.username === body.credential;
   // });
 
-  const user = await UserModel.find({
-    $or: [
-      { email: body.credential },
-      { phone: body.credential },
-      { username: body.credential },
-    ],
-  });
+ 
+  const user = await UserModel.findOne({ $or: [{ email: body.credential }, { phone: body.credential }, { username: body.credential }] });
 
   if (!user) {
     return res.status(400).send({ message: "Wrong credentials!" });
   }
+
+  console.log("User password:", user.password);
+  console.log("Body password:", body.password);
 
   const isCorrectPassword = bcrypt.compareSync(body.password, user.password);
 
@@ -131,8 +141,6 @@ app.post("/signin", async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  mongoose.connect(
-    "mongodb+srv://dashoe681_db_user:o9MjlYQiO0b0QeUb@cluster0.knkcfnh.mongodb.net/instagram"
-  );
-  console.log(`Your app is running on http://localhost:${PORT}`);
+  mongoose.connect(process.env.MONGO_URL);
+  console.log(`Your  app is running on http://localhost:${PORT}`);
 });
