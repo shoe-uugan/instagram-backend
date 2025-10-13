b7d5dcc3e37d9bf4bc1be879bf243e99472f7056
25HP3408 25HP3408 <25HP3408@G301-CC0192.local>
2025-10-13 18:28:59 +0800

    comment

---
+++ models/post-comment.model.mjs
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { nanoid } from "nanoid";
+
+const CommentSchema = new mongoose.Schema(
+  {
+    _id: { type: String, default: nanoid() },
+    post: { type: String, ref: "Post" },
+    description: { type: String },
+    createdBy: { type: String, ref: "User" },
+    createdAt: { type: Date },
+    updatedAt: { type: Date },
+  },
+  { timestamps: true }
+);
+
+
+
+export const CommentModel = mongoose.model("Post", CommentSchema);
